dad377dfe4e52a15e16709fac385c6c7abb0f322
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-08 22:39:43 +0300

    update style

--- index.php
+++ index.php
@@ -17,52 +17,75 @@
         </div>
     </header>
     <main>
-        <div class="Date incomeData">
-            <div class="tittleDate">Доход</div>
-            <select class="incomeName">
-                <option value="1">Зарплата(Ованс)</option>
-                <option value="2">Зарплата</option>
-                <option value="3">ИП</option>
-                <option value="4">Другое</option>
-            </select>
-            <input type="text" class="sumIncome" placeholder="СУММА">
-            <textarea class="description" placeholder="Описание"></textarea>
-            <button class="btnSendIncome">Отправить</button> 
+        <div class="infoMain btnPanel">
+            <button class="btnMenu">Send</button>
+            <button class="btnMenu">Send</button>
+            <button class="btnMenu">Send</button>
+            <button class="btnMenu">Send</button>
         </div>
-        <div class="Date expenseData">
-            <div class="tittleDate">Расход</div>
-            <select class="expenseName">
-                <option value="1">Зарплата(Ованс)</option>
-                <option value="2">Зарплата</option>
-                <option value="3">ИП</option>
-                <option value="4">Другое</option>
-            </select>
-            <input type="text" class="sumExpense" placeholder="СУММА">
-            <textarea class="description" placeholder="Описание"></textarea>
-            <button class="btnSendExpense">Отправить</button>
+        <div class="infoMain sendInfo">
+            <div class="Date incomeData">
+                <div class="tittleData incomeTitle">Доход</div>
+                <select class="incomeName">
+                    <option value="1">Зарплата(Ованс)</option>
+                    <option value="2">Зарплата</option>
+                    <option value="3">ИП</option>
+                    <option value="4">Другое</option>
+                </select>
+                <input type="text" class="sumIncome" placeholder="СУММА">
+                <textarea class="description" placeholder="Описание"></textarea>
+                <button class="btnSendIncome">Отправить</button> 
+            </div>
+            <div class="Date expenseData">
+                <div class="tittleData expenseTitle">Расход</div>
+                <select class="expenseName">
+                    <option value="1">Зарплата(Ованс)</option>
+                    <option value="2">Зарплата</option>
+                    <option value="3">ИП</option>
+                    <option value="4">Другое</option>
+                </select>
+                <input type="text" class="sumExpense" placeholder="СУММА">
+                <textarea class="description" placeholder="Описание"></textarea>
+                <button class="btnSendExpense">Отправить</button>
+            </div>
+            <div class="operations">
+                <table>
+                    <tr>
+                        <td>Дата</td>
+                        <td>СУММА</td>
+                        <td>Операция</td>
+                        <td>Имя журнала учёта</td>
+                    </tr>
+                    <tr>
+                        <td>08.01.2023</td>
+                        <td>1000</td>
+                        <td>Доход</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>08.01.2023</td>
+                        <td>500</td>
+                        <td>Расход</td>
+                        <td>Авто</td>
+                    </tr>
+                </table>
+            </div>
         </div>
-        <div class="operations">
-            <table>
-                <tr>
-                    <td>Дата</td>
-                    <td>СУММА</td>
-                    <td>Операция</td>
-                    <td>Имя журнала учёта</td>
-                </tr>
-                <tr>
-                    <td>08.01.2023</td>
-                    <td>1000</td>
-                    <td>Доход</td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>08.01.2023</td>
-                    <td>500</td>
-                    <td>Расход</td>
-                    <td>Авто</td>
-                </tr>
-            </table>
+        <div class="infoMain bdInfo">
+            <div class="itemBd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ipsa. Cum voluptas accusamus, consequatur veniam harum, facilis, quaerat neque inventore vero sint sed corrupti provident.</p>
+            </div>
+            <div class="itemBd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ipsa. Cum voluptas accusamus, consequatur veniam harum, facilis, quaerat neque inventore vero sint sed corrupti provident.</p>
+            </div>
+            <div class="itemBd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ipsa. Cum voluptas accusamus, consequatur veniam harum, facilis, quaerat neque inventore vero sint sed corrupti provident.</p>
+            </div>
+            <div class="itemBd">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ipsa. Cum voluptas accusamus, consequatur veniam harum, facilis, quaerat neque inventore vero sint sed corrupti provident.</p>
+            </div>
         </div>
+        
     </main>
 
     <script type="module" src="js/index.js"></script>
