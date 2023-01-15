b304a91651b7e6e371e01ad9e44f96c7a2d8e123
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-15 16:04:56 +0300

    update js, css, html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 import * as sendOperation from "./contentOperation.js";
+// анимация блока DATA
+const TIT_DATE = document.querySelectorAll(".tittleData");
+const DATA_BLOCK = document.querySelectorAll(".Data");
+const INFO_BLOCK= document.querySelectorAll(".Info");
+
+function animTitDat(dataB, infoB){  
+    TIT_DATE.forEach(tit => {
+        tit.addEventListener('click', ()=>{
+            console.log("click title");
+        });
+    });
+}
+animTitDat(DATA_BLOCK, INFO_BLOCK);
