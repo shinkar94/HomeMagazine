4c181135f2ca9af997a0f5f88c21fa074d05b1de
shinkar94 <romka94shinkarenko@gmail.com>
2023-01-16 21:54:04 +0300

    update html,css

--- index.php
+++ index.php
@@ -33,86 +33,96 @@
             <button class="btnMenu">Send</button>
         </div>
         <div class="infoMain sendInfo">
-            <div class="Data_Info">
-                <div class="Info remainderData">
-                    <div class="tittleData title"><img src="img/icon/money2.png" alt="imgTitle">Остаток</div>
-                    <div class="countMoney"><img src="img/flag/byn.png" alt="flagMoney"><p>'.$countDb['countMoney'].'</p><p class="flagName">BYN</p></div>
-                    <div class="countMoney"><img src="img/flag/usd.png" alt="flagMoney"><p>'.round($countDb['countMoney'] / 2.63).'</p><p class="flagName">USD</p></div>
-                    <div class="countMoney"><img src="img/flag/eur.png" alt="flagMoney"><p>'.round($countDb['countMoney'] / 2.80).'</p><p class="flagName">EUR</p></div>
-                    <div class="countMoney"><img src="img/flag/rub.png" alt="flagMoney"><p>'.round(($countDb['countMoney'] / 3.8) * 100).'</p><p class="flagName">RUB</p></div>
+            <div class="containerInfos">
+                <div class="Infos_Panel">
+                    <div class="Info remainderData">
+                        <div class="tittleData title"><img src="img/icon/money2.png" alt="imgTitle">Остаток</div>
+                        <div class="countMoney"><img src="img/flag/byn.png" alt="flagMoney"><p>'.$countDb['countMoney'].'</p><p class="flagName">BYN</p></div>
+                        <div class="countMoney"><img src="img/flag/usd.png" alt="flagMoney"><p>'.round($countDb['countMoney'] / 2.63).'</p><p class="flagName">USD</p></div>
+                        <div class="countMoney"><img src="img/flag/eur.png" alt="flagMoney"><p>'.round($countDb['countMoney'] / 2.80).'</p><p class="flagName">EUR</p></div>
+                        <div class="countMoney"><img src="img/flag/rub.png" alt="flagMoney"><p>'.round(($countDb['countMoney'] / 3.8) * 100).'</p><p class="flagName">RUB</p></div>
+                    </div>
+                    <div class="Info remainderData">
+                        <div class="tittleData title"><img src="img/icon/card.png" alt="imgTitle"> Карта</div>
+                        <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>173</p><p class="flagName">BYN</p></div>
+                        <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
+                        <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
+                        <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
+                    </div>
+                    <div class="Info remainderData">
+                        <div class="tittleData title"><img src="img/icon/money.png" alt="imgTitle"> Наличные</div>
+                        <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>100</p><p class="flagName">BYN</p></div>
+                        <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
+                        <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
+                        <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
+                    </div>
+                    <div class="Info remainderData">
+                        <div class="tittleData title"><img src="img/icon/money2.png" alt="imgTitle"> Падушка</div>
+                        <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>400</p><p class="flagName">BYN</p></div>
+                        <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
+                        <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
+                        <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
+                    </div>
                 </div>
-                <div class="Info remainderData">
-                    <div class="tittleData title"><img src="img/icon/card.png" alt="imgTitle"> Карта</div>
-                    <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>173</p><p class="flagName">BYN</p></div>
-                    <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
-                    <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
-                    <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
+            </div>
+            <div class="containerData">
+                <div class="Data_Info">
+                    <div class="Data incomeData">
+                        <div class="tittleData title"><img src="img/icon/up.png" alt="imgTitle">Доход</div>
+                        <input type="date" class="dateSend">
+                        <select class="opirationName">
+                            <option></option>
+                            <option>Зарплата(Ованс) Рома</option>
+                            <option>Зарплата(Ованс) Катя</option>
+                            <option>Зарплата Рома</option>
+                            <option>Зарплата Катя</option>
+                            <option>ИП</option>
+                            <option>Другое</option>
+                        </select>
+                        <input type="text" class="sum" placeholder="СУММА">
+                        <textarea class="description" placeholder="Описание"></textarea>
+                        <button class="btnSend incomeBtn">Отправить</button> 
+                    </div>
+                    <div class="Data expenseData">
+                        <div class="tittleData title"><img src="img/icon/not.png" alt="imgTitle">Расход</div>
+                        <input type="date" class="dateSend">
+                        <select class="opirationName">
+                            <option></option>
+                            <option>Продукты</option>
+                            <option>Бытовая химия</option>
+                            <option>Зверюшки</option>
+                            <option>Здоровье</option>
+                            <option>Кредиты и Рассрочки</option>
+                            <option>Жильё</option>
+                            <option>Топливо</option>
+                            <option>Авто</option>
+                            <option>Бьюти</option>
+                            <option>Образование</option>
+                            <option>Подарки</option>
+                            <option>Такси</option>
+                            <option>Кафе</option>
+                            <option>Другое</option>
+                        </select>
+                        <input type="text" class="sum" placeholder="СУММА">
+                        <textarea class="description" placeholder="Описание"></textarea>
+                        <button class="btnSend expenseBtn">Отправить</button>
+                    </div>
                 </div>
-                <div class="Info remainderData">
-                    <div class="tittleData title"><img src="img/icon/money.png" alt="imgTitle"> Наличные</div>
-                    <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>100</p><p class="flagName">BYN</p></div>
-                    <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
-                    <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
-                    <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
-                </div>
-                <div class="Info remainderData">
-                    <div class="tittleData title"><img src="img/icon/money2.png" alt="imgTitle"> Падушка</div>
-                    <div class="countMoney"><img src="img/flag/byn.png" alt=""><p>400</p><p class="flagName">BYN</p></div>
-                    <div class="countMoney"><img src="img/flag/usd.png" alt=""><p>104</p><p class="flagName">USD</p></div>
-                    <div class="countMoney"><img src="img/flag/eur.png" alt=""><p>98</p><p class="flagName">EUR</p></div>
-                    <div class="countMoney"><img src="img/flag/rub.png" alt=""><p>7000</p><p class="flagName">RUB</p></div>
-                </div>
-                <div class="Data incomeData">
-                    <div class="tittleData title"><img src="img/icon/up.png" alt="imgTitle">Доход</div>
-                    <input type="date" class="dateSend">
-                    <select class="opirationName">
-                        <option></option>
-                        <option>Зарплата(Ованс) Рома</option>
-                        <option>Зарплата(Ованс) Катя</option>
-                        <option>Зарплата Рома</option>
-                        <option>Зарплата Катя</option>
-                        <option>ИП</option>
-                        <option>Другое</option>
-                    </select>
-                    <input type="text" class="sum" placeholder="СУММА">
-                    <textarea class="description" placeholder="Описание"></textarea>
-                    <button class="btnSend incomeBtn">Отправить</button> 
-                </div>
-                <div class="Data expenseData">
-                    <div class="tittleData title"><img src="img/icon/not.png" alt="imgTitle">Расход</div>
-                    <input type="date" class="dateSend">
-                    <select class="opirationName">
-                        <option></option>
-                        <option>Продукты</option>
-                        <option>Бытовая химия</option>
-                        <option>Зверюшки</option>
-                        <option>Здоровье</option>
-                        <option>Кредиты и Рассрочки</option>
-                        <option>Жильё</option>
-                        <option>Топливо</option>
-                        <option>Авто</option>
-                        <option>Бьюти</option>
-                        <option>Образование</option>
-                        <option>Подарки</option>
-                        <option>Такси</option>
-                        <option>Кафе</option>
-                        <option>Другое</option>
-                    </select>
-                    <input type="text" class="sum" placeholder="СУММА">
-                    <textarea class="description" placeholder="Описание"></textarea>
-                    <button class="btnSend expenseBtn">Отправить</button>
-                </div>
-                <div class="Info translationData">
-                    <div class="tittleData title"><img src="img/icon/nn.png" alt="imgTitle"> Перевод</div>
-                    <select class="opirationName">
-                        <option value="1">В наличные</option>
-                        <option value="2">На карту</option>
-                        <option value="3">На падушку</option>
-                        <option value="4">Долг</option>
-                    </select>
-                    <input type="text" class="sum" placeholder="СУММА">
-                    <textarea class="description" placeholder="Описание"></textarea>
-                    <button class="btnSend">Отправить</button> 
+            </div>
+            <div class="containerTranslation">
+                <div class="translationPanel">
+                    <div class="translationData">
+                        <div class="tittleData title"><img src="img/icon/nn.png" alt="imgTitle"> Перевод</div>
+                        <select class="opirationName">
+                            <option value="1">В наличные</option>
+                            <option value="2">На карту</option>
+                            <option value="3">На падушку</option>
+                            <option value="4">Долг</option>
+                        </select>
+                        <input type="text" class="sum" placeholder="СУММА">
+                        <textarea class="description" placeholder="Описание"></textarea>
+                        <button class="btnSend">Отправить</button> 
+                    </div>
                 </div>
             </div>
             <div class="operations">';
